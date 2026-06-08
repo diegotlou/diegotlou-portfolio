07fb5a982a42c639829ccea7a4e409ff54bbc649
Diego <diego13579dmc@gmail.com>
2026-06-07 21:26:18 -0600

    refactor: add new index.js for images

---
+++ src/assets/images/index.js
@@ -0,0 +1,39 @@
+import imagenTableroUno from "./proyects/tablero/1.png";
+import imagenTableroDos from "./proyects/tablero/2.png";
+import imagenTableroTres from "./proyects/tablero/3.png";
+import imagenTableroCuatro from "./proyects/tablero/4.png";
+import imagenScrollerUno from "./proyects/scroller/1.png";
+import imagenScrollerDos from "./proyects/scroller/2.png";
+import imagenScrollerTres from "./proyects/scroller/3.png";
+import imagenScrollerCuatro from "./proyects/scroller/4.png";
+import imagenPaisesUno from "./proyects/paises/1.png";
+import imagenPaisesDos from "./proyects/paises/2.png";
+import imagenPaisesTres from "./proyects/paises/3.png";
+import imagenPaisesCuatro from "./proyects/paises/4.png";
+import imagenPaisesCinco from "./proyects/paises/5.png";
+import imagenRecomendacionUno from "./proyects/recomendacion/1.png";
+import imagenRecomendacionDos from "./proyects/recomendacion/2.png";
+import imagenRagUno from "./proyects/rag/1.png";
+import imagenRagDos from "./proyects/rag/2.png";
+import imagenRagTres from "./proyects/rag/3.png";
+
+export {
+    imagenTableroUno,
+    imagenTableroDos,
+    imagenTableroTres,
+    imagenTableroCuatro,
+    imagenScrollerUno,
+    imagenScrollerDos,
+    imagenScrollerTres,
+    imagenScrollerCuatro,
+    imagenPaisesUno,
+    imagenPaisesDos,
+    imagenPaisesTres,
+    imagenPaisesCuatro,
+    imagenPaisesCinco,
+    imagenRecomendacionUno,
+    imagenRecomendacionDos,
+    imagenRagUno,
+    imagenRagDos,
+    imagenRagTres,
+};
